474c8abfcb21b703f8ccb5c864455a306ccd8d72
Kaique araujo lopes <kaiquearaujo246@hotmail.com>
2023-05-30 10:28:19 -0300

    finished page home

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
+import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from "styled-components"
-import { defaultTheme } from  "./styles/themes/default"
+import { defaultTheme } from "./styles/themes/default"
 import { GlobalStyle } from "./styles/global"
+import { Router } from "./Router"
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <div>
-        <h1>Olá mundo!</h1>
-      </div>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
 
       <GlobalStyle />
     </ThemeProvider>
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,47 @@
+import { HomeContainer, CountdownContainer, FormContainer, Separato, StartCountdownButton, TaskInput, MinutosAmountInput } from "./styles";
+
+import { Play } from "@phosphor-icons/react";
+
+export function Home() {
+    return (
+        <HomeContainer>
+            <form>
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput
+                        id="task"
+                        type="text"
+                        placeholder="Dê um nome para o seu projeto"
+                    />
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutosAmountInput
+                        id="minutesAmount"
+                        type="number"
+                        placeholder="00"
+                    />
+
+                    <span>minutos</span>
+                </FormContainer>
+
+                <CountdownContainer>
+                    <span>0</span>
+                    <span>0</span>
+
+                    <Separato>:</Separato>
+
+                    <span>0</span>
+                    <span>0</span>
+                </CountdownContainer>
+
+                <StartCountdownButton
+                    type="submit"
+                    disabled
+                >
+                    <Play />
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
